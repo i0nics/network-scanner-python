f2b3877e7a24f95c0bf4462d53ab25b1bc01f0c0
Bikram Chatterjee <56491363+i0nics@users.noreply.github.com>
2021-02-15 09:10:10 +0530

    Update Comments

--- networkdiscovery.py
+++ networkdiscovery.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python3
 # Programmer: Bikram Chatterjee
 # Network Discovery
-# Description: This program lists all available interfaces present in the host’s LAN and uses Scapy to identify all
-# online machines accessible through each of the host’s non-virtual interfaces. To achieve this, this program
-# creates an ethernet frame which encapsulates an ARP packet. The ethernet frame has its destination address set to the
-# broadcast address while the ARP packet has the host's subnet address along with the CIDR notation. Finally,
-# Scapy creates a list of ARP packets which are then broadcasted to every IP address in the subnet and prints the MAC
-# and IP addresses of the online machines which send a reply back to the host.
+'''
+This program lists all available interfaces present in the host’s LAN and uses Scapy to identify all
+online machines accessible through each of the host’s non-virtual interfaces. To achieve this, this program
+creates an ethernet frame which encapsulates an ARP packet. The ethernet frame has its destination address set to the
+broadcast address while the ARP packet has the host's subnet address along with the CIDR notation. Finally,
+Scapy creates a list of ARP packets which are then broadcasted to every IP address in the subnet and prints the MAC
+and IP addresses of the online machines which send a reply back to the host.
+'''
 from scapy.all import *
 import argparse
 import netifaces
@@ -35,18 +37,23 @@ def main():
                        netifaces.ifaddresses(i)[netifaces.AF_INET][0].get('netmask').split('.'))
             print(f'\n* {i}:' + ' ' * (max_space - len(i)), f'{get_if_hwaddr(i)}   IP = {get_if_addr(i)}/{cidr}',
                   end='')
-            non_virt_ifaces.append(i) if get_if_hwaddr(i) != '00:00:00:00:00:00' else 0  # Filter Non-Virtual Interfaces
+           
+            # Filter Non-Virtual Interfaces
+            non_virt_ifaces.append(i) if get_if_hwaddr(i) != '00:00:00:00:00:00' else 0  
+            
         except (KeyError, Scapy_Exception) as e:
             print(f'\n* {i}:' + ' ' * (max_space - len(i)), f'00:00:00:00:00:00   IP = {get_if_addr(i)} ', end='')
 
     # Identify All Online Interfaces Accessible Through The Non-Virtual Interfaces of Local Machine
     for i in non_virt_ifaces:
         print(f'\n----\nScanning on Interface {i}\n----\nResults:')
+       
         # Get The Subnet Size of The Interface Used to Probe The Network in CIDR notation via Netifaces
-        cidr = sum(
-            bin(int(x)).count('1') for x in netifaces.ifaddresses(i)[netifaces.AF_INET][0].get('netmask').split('.'))
+        cidr = sum(bin(int(x)).count('1') for x in netifaces.ifaddresses(i)[netifaces.AF_INET][0].get('netmask').split('.'))
+       
         # Encapsulate ARP packet in Ethernet Frame with Destination MAC Address Set to Broadcast Address
         request = Ether(dst='ff:ff:ff:ff:ff:ff') / ARP(pdst=f'{get_if_addr(i)}/{cidr}')
+        
         # Send and Receive the Data Link Layer Packets
         # Timeout to Receive Packets Set to 2 seconds and Unanswered Packets Are Sent One More Time via Retry
         # Received Packets are Separated into Answered and Unanswered Packets
